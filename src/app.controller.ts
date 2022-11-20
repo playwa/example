f2aa434a1192cf0ff991ea0ff679d7bbaf418593
Nata4n <natan-sr@hotmail.com>
2022-11-20 08:20:20 -0300

    feat: add event example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,8 @@ import {
   Param,
   Command,
   Controller,
+  On,
+  WAEvent
 } from '@playwa/common';
 
 import { AppService } from './app.service';
@@ -13,5 +15,10 @@ export class AppController {
   @Command('/say :text')
   async say(@Param('text') text: string) {
     return this.appService.say(text);
+  }
+
+  @On(WAEvent.MESSAGES_UPSERT)
+  async onMessage(msg) {
+    console.log(msg.messages[0]);
   }
 }
